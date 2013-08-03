23f9e3b4026827e17390bd12e12a3a532b2e3e18
westjeffho_cp <SND\westjeffho_cp>
2013-08-03 04:05:22 +0000

    init check in

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Test.Properties;
+using TestDatabase;
+
+namespace NewNew
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = new OracleConnection(Settings.Default.DB))
+            {
+                var db = new DbContext(conn,Console.Out);
+
+                var query = from a in db.N_USER
+                            let b = a.AD_FLAG
+                            where a.LANG == null
+                            where a.LOCK_FLAG == Flag.N
+                            select new
+                            {
+                                a.USER_ID,
+                                a.USER_NAME,
+                                a.UPDATE_DATE,
+                                b,
+                            };
+
+                // flowing statement will compiler error:
+                //var q = from a in db.N_USER
+                //        group new { a.DEPT_CODE, a.LANG} by a.DEPT_CODE into g
+                //        select new
+                //        {
+                //            Department = g.Key,
+                //            Count = g.Count(),
+                //        };
+            }
+        }
+
+    }
+
+}
