982b72867058642d05e522065c66e89543cae71e
westjeffho_cp <SND\westjeffho_cp>
2013-08-21 15:23:39 +0000

    incomplete. need more work & attempt

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Linq2Oracle;
+using Linq2Oracle.Expressions;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Test.DataModel;
+using Test.DataModel.Code;
+using Test.Properties;
+
+namespace NewNew
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "User Id=username;Password=passwd;data source=//server:port/SID";
+            using (var conn = new OracleConnection(connectionString))
+            {
+                var db = new DbContext(conn, Console.Out);
+                int? q1 = db.N_USER.Max(a => a.LOGIN_COUNT);
+
+
+                int? i = null;
+                var q2 = db.N_USER
+                            .Where(a => a.DEPT_CODE + "A" == null)
+                            .Where(a => a.LOGIN_COUNT.GetValueOrDefault(0) == i);
+                            //.GroupBy(a => a.DEPT_CODE)
+                            //.Where(g => g.Count() == 1) //&& g.Sum(a=>a.LOGIN_COUNT) >100)
+                            //.Select(g => new
+                            //{
+                            //    Department = g.Key,
+                            //    Count = g.Count(),
+                            //});
+            }
+        }
+
+    }
+
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataAccessLib.LinqPadDriver")]
+[assembly: AssemblyTitle("Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAccessLib.LinqPadDriver")]
+[assembly: AssemblyProduct("Test")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc97eb29-0720-4e12-b192-20c1b339dd8a")]
+[assembly: Guid("32a7d0e6-d52c-4233-b54c-dabe46d70019")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.6")]
-[assembly: AssemblyFileVersion("1.0.4.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
