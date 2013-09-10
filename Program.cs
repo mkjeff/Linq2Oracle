47f668b549d7e75bfd55ec29d7a94b98c664013e
westjeffho_cp <SND\westjeffho_cp>
2013-09-10 11:06:45 +0000

    Optional predicate extension method sample

--- Program.cs
+++ Program.cs
@@ -13,20 +13,41 @@ namespace NewNew
 {
     static class Program
     {
+        static T Then<T>(this bool condition, Func<T> valueProvider) where T:struct
+        {
+            return condition ? valueProvider() : default(T);
+        }
+
+        static bool HasValue(this string str)
+        {
+            return !string.IsNullOrEmpty(str);
+        }
+
         static void Main(string[] args)
         {
             const string connectionString = "User Id=username;Password=passwd;data source=//server:port/SID";
             using (var conn = new OracleConnection(connectionString))
             {
+                var dept = "";
                 var db = new DbContext(conn,Console.Out);
                 // flowing statement will compiler error:
                 var q = from a in db.N_USER
+                        where dept.HasValue().Then(() => a.DEPT_CODE == dept) // optional predicate when dept.HasValue()
                         group a by a.DEPT_CODE into g
+                        where g.Any(a => a.AD_FLAG == Flag.N)
                         select new
                         {
                             Department = g.Key,
                             Count = g.Count(),
                         };
+
+                var q2 = from a in db.N_USER
+                         group a by a.DEPT_CODE;
+
+                foreach (var g in q2)
+                {
+                    var q21 = g.Where(a => a.DEPT_CODE == "");
+                }
             }
         }
 
